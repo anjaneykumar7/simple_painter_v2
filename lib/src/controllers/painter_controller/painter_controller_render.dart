294d245cbce2c3d04bfea699eced2bf01aeeb037
Ataberk K <ataberkw@gmail.com>
2025-03-06 14:40:41 +0100

    feat: add pixelRatio option on renderImage function

--- lib/src/controllers/painter_controller/painter_controller_render.dart
+++ lib/src/controllers/painter_controller/painter_controller_render.dart
@@ -3,13 +3,13 @@ part of 'painter_controller.dart';
 extension PainterControllerRender on PainterController {
   /// Renders the current painting as an image and returns it as a Uint8List.
   /// This can be used to save or share the painted content.
-  Future<Uint8List?> renderImage() async {
+  Future<Uint8List?> renderImage({double pixelRatio = 1.0}) async {
     try {
       clearSelectedItem(); // Deselect any selected item before rendering.
       await Future.delayed(const Duration(milliseconds: 100), () {});
       final boundary = repaintBoundaryKey.currentContext!.findRenderObject()!
           as RenderRepaintBoundary;
-      final image = await boundary.toImage();
+      final image = await boundary.toImage(pixelRatio: pixelRatio);
       final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
       return byteData?.buffer.asUint8List();
     } catch (e) {
